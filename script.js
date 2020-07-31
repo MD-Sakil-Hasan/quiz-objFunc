02357d49986a4e4e3b1de22d1832de3d4a0f2937
MD-Sakil-Hasan <mdsakilhasan833@gmail.comm>
2020-07-31 14:15:40 +0600

    Challenge Completed

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ c) correct answer (I would use a number for this)
 
 //setting IIFE to make the code private
 
-
+/*
 (function(){
     function Question(question, answers, correct){
         this.question = question;
@@ -74,5 +74,102 @@ c) correct answer (I would use a number for this)
     
     questions[n].checkAnswer(answer)
 })()
+*/
 
 
+
+
+
+/*
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
+
+
+
+
+
+
+(function(){
+    function Question(question, answers, correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+    
+    Question.prototype.displayQuestion = function(){
+        console.log(this.question)
+    
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.checkAnswer = function(ans, callback){
+        var sc;
+
+        if(ans === this.correct){
+            console.log('Correct Answer')
+            sc = callback(true)
+        } else {
+            console.log('Wrong Answer! Try Again')
+            sc = callback(false)
+        }
+        this.displayScore(sc)
+    }
+
+    Question.prototype.displayScore = function(score){
+        console.log('Your current score is ' + score)
+        console.log('--------------------------------------------------------------')
+    }
+    
+    
+    var q1 = new Question('Is javascript is the coolest programming language in the world',
+    ['Yes', 'No'],
+    0)
+    
+    var q2 = new Question('What does best describe coding?',
+    ['Boring', 'Hard', 'Fun', 'Tedious'],
+    2)
+    
+    var q3 = new Question('What is Sakil\'s most favorite food?',
+    ['Vegetables', 'Beef Biriyani', 'Mutton Burger'],
+    1)
+    
+    
+    
+    var questions = [q1, q2, q3]
+
+
+    function score(){
+        var sc = 0;
+        return function (correct){
+            if (correct){
+                sc++
+            }
+            return sc
+        }
+    }
+    var keepScore = score()
+
+    
+    function nextQuestion(){
+        var n = Math.floor(Math.random() * questions.length)
+    
+        questions[n].displayQuestion()
+        
+        
+        var answer = prompt('Please provide the correct answer');
+        
+        if(answer !== 'exit'){
+            questions[n].checkAnswer(parseInt(answer), keepScore)
+
+            nextQuestion()
+        }
+    }
+
+    nextQuestion()
+})()
